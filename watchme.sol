a9ce525a8c054a2db8f8ffd6b454d49a229507fe
AGWeb09 <ag.web09@gmail.com>
2021-09-07 16:13:06 +0200

    Update watchme.sol

--- watchme.sol
+++ watchme.sol
@@ -6,12 +6,19 @@ contract MiraMe{
     
     
     // Events
+    /*
+    * New Content by 'creator' has been added. It has id 'id'
+    */
     event NewContent(uint id, address creator);
+    /*
+    * Content 'id', created by 'creator' change sponsor to 'sponsor'
+    */ 
     event ChangeSponsor(uint id, address creator, address sponsor);
     
     // constants
     uint sponsorPercentageOnSell = 70;
     uint creatorPercentageOnSell = 20;
+    uint minimumFee = 0.0001 ether;
     
     // structs
     struct Content{
@@ -22,30 +29,30 @@ contract MiraMe{
     address platform;
     Content[] public contents;
     
-    mapping (uint => address) public contentToOwner;
-    mapping (uint => address) public contentToSponsor;
-    mapping (address => uint) public contentsByOwner;
+    mapping (uint => address) private contentToOwner;
+    mapping (uint => address) private contentToSponsor;
+    mapping (address => uint) private contentsByOwner;
     
     
     // Modifiers
     
     modifier onlyCreator(uint id, address _creator){
-        require(contentToOwner[id] == _creator);
+        require(contentToOwner[id] == _creator, "Only Creator can do this");
         _;
     }
     
     modifier onlySponsor(uint id, address _sponsor){
-        require(contentToSponsor[id] == _sponsor);
+        require(contentToSponsor[id] == _sponsor, "Only Sponsor can do this");
         _;
     }
     
     modifier onlyCreatorOrSponsor(uint id, address _creatorOrSponsor){
-        require(contentToOwner[id] == _creatorOrSponsor || contentToSponsor[id] == _creatorOrSponsor);
+        require(contentToOwner[id] == _creatorOrSponsor || contentToSponsor[id] == _creatorOrSponsor, "Only Creator or Sponsor can do this");
         _;
     }
     
     modifier onlyPlatform(){
-        require(msg.sender == platform);
+        require(msg.sender == platform, "Only ME can do this.");
         _;
     }
     
@@ -72,22 +79,43 @@ contract MiraMe{
     
     // Public functions
     
+    /*
+    * Creates a new Content with a given hash and a given price
+    * Price has to be higher than minimumFee saved in the contract.
+    * Price has to be in Wei.
+    */ 
     function createContent(string calldata hash, uint price) public {
-        require(price > 0.0001 ether);
+        require(price > minimumFee, "Price is too low");
         address owner = msg.sender;
         _createContent(hash, price, owner);
     }
     
-    function changeSponsor(uint id, address newSponsor) public onlyCreatorOrSponsor(id, msg.sender){
+    /*
+    * Changes the sponsor of the Content
+    * Only the sponsor can change the sponsor. 
+    * At the beginning, the creator IS the sponsor. 
+    */
+    function changeSponsor(uint id, address newSponsor) public onlySponsor(id, msg.sender){
         _changeSponsor(id, newSponsor);
     }
     
+    /*
+    * Changes the price of the content.
+    * Price has to be higher than minimuFee saved in the contract.
+    * Price has to be in Wei.
+    * Only the sponsor can change the sponsor.
+    * At the beginning, the creator IS the sponsor.
+    */
     function changePrice(uint id, uint price) public onlySponsor(id, msg.sender){
+        require(price > minimumFee, "New Price is too low");
         contents[id].price = price;
     }
     
+    /*
+    * Buy the Content, providing the value in wei.
+    */
     function buyContent(uint _id) public payable {
-        require(contents[_id].price <= msg.value);
+        require(contents[_id].price >= msg.value, "Your offer is below price");
         uint creatorFee = msg.value * creatorPercentageOnSell / 100;
         uint sponsorFee = msg.value * sponsorPercentageOnSell / 100;
         uint platformFee = msg.value - creatorFee - sponsorFee;
@@ -95,8 +123,13 @@ contract MiraMe{
         payable(contentToOwner[_id]).transfer(creatorFee);
         payable(contentToSponsor[_id]).transfer(sponsorFee);
         payable(platform).transfer(platformFee);
+        
+        contentToSponsor[_id] = msg.sender;
     }
     
+    /*
+    * Get the list of the contents by creator.
+    */
     function getContentsByCreator(address _creator) external view returns(uint[] memory) {
         uint[] memory result = new uint[](contentsByOwner[_creator]);
         uint counter = 0;
@@ -109,17 +142,33 @@ contract MiraMe{
         return result;
     }
     
-    
-    function changePercentages(uint sponsor, uint creator) public onlyPlatform {
-        require(sponsor + creator <= 95);
-        sponsorPercentageOnSell = sponsor;
-        creatorPercentageOnSell = creator;
+    /*
+    * Changes the percentage for all the contents.
+    * The sponsor percentage and the creator percentage cannot overflow 95, for a minimum 5% percentage for the platform.
+    */
+    function changePercentages(uint _sponsor, uint _creator) public onlyPlatform {
+        require(_sponsor + _creator <= 95, "Sponsor + Creator fees are too high");
+        sponsorPercentageOnSell = _sponsor;
+        creatorPercentageOnSell = _creator;
     }
     
+    /*
+    * Changes the minimum fee to operate.
+    */
+    function changeMinimumFee(uint _minimumFee) public onlyPlatform{
+        minimumFee = _minimumFee;
+    }
+    
+    /*
+    * Gives back how many is stored in the contract.
+    */
     function getValuesStored() public view onlyPlatform returns(uint){
         return address(this).balance;
     } 
     
+    /*
+    * Get back all the money forom the contract.
+    */
     function transferMoney() public onlyPlatform{
         payable(platform).transfer(address(this).balance);
     }
@@ -127,7 +176,5 @@ contract MiraMe{
     receive() external payable {
         payable(platform).transfer(msg.value);
     }
-    
-    
     
 }
